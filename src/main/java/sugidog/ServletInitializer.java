801adbb8a44f57b0ac86b795163a71eb719f62d1
utotty <utotty+sugidog@gmail.com>
2022-10-27 18:22:29 +0900

    source add.

---
+++ src/main/java/sugidog/ServletInitializer.java
@@ -0,0 +1,13 @@
+package sugidog;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(SugidogApplication.class);
+	}
+
+}
